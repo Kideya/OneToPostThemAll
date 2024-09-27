409fd442a0843ff56ace9f43911a212cf6457cfa
Kideya <officialfelixkranz@gmx.de>
2024-09-27 17:36:55 +0200

    Changed link for the WMO translation

--- UsefullCommands.py
+++ UsefullCommands.py
@@ -19,7 +19,7 @@ def Weather(bot):
     async def Wetter(ctx):       
         #Links for APIs --> Variable Names
         wetterAPILink = 'https://api.open-meteo.com/v1/forecast?latitude=52.1277&longitude=11.6292&current=temperature_2m,weather_code&forecast_days=1&models=icon_seamless'
-        WMOTranslatorLink = 'https://gist.githubusercontent.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c/raw/76b0cb0ef0bfd8a2ec988aa54e30ecd1b483495d/descriptions.json'
+        WMOTranslatorLink = 'https://raw.githubusercontent.com/Kideya/OneToPostThemAll/refs/heads/main/WMO_translation_de.json'
         
         #fetching data from the weather api
         responseWeather_API = requests.get(wetterAPILink).json()
